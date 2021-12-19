24dc7c3ffcd79e7fbef10a528a513b7eea28df58
Jørgen Orehøj Erichsen <joe@erichsen.net>
2021-12-19 19:15:49 +0100

    This is prettier

--- lib/cookbook_web/live/front_page_live.ex
+++ lib/cookbook_web/live/front_page_live.ex
@@ -6,25 +6,62 @@ defmodule CookbookWeb.FrontPageLive do
     <h1>Welcome to the Phoenix LiveView Cookbook</h1>
     <h3>This contains examples of using Phoenix LiveView</h3>
 
-    <p>The plan is to have the following examples:</p>
+    <div class="row">
+      <div class="col">Hello World</div>
+      <div class="col">
+        <a href="/hello_world" class="btn btn-outline-success ml-5">Demo</a>
+        <a href="https://github.com/joerichsen/live_view_cookbook/blob/main/lib/cookbook_web/live/hello_world_live.ex" class="btn btn-outline-primary">Code</a>
+      </div>
+    </div>
 
-    <p>
-      Hello World
-      <a href="https://liveviewcookbook.dev/hello_world" class="btn btn-outline-success ml-5">Demo</a>
-      <a href="https://github.com/joerichsen/live_view_cookbook/blob/main/lib/cookbook_web/live/hello_world_live.ex" class="btn btn-outline-primary">Code</a>
-    </p>
+    <div class="row pt-3">
+      <div class="col">Form with validation</div>
+      <div class="col">TODO</div>
+    </div>
 
-    <p>Form with validation (TODO)</p>
-    <p>Dynamic nested form (TODO)</p>
-    <p>Multistep wizard form (TODO)</p>
-    <p>File upload (TODO)</p>
-    <p>File upload to S3 (TODO)</p>
-    <p>Picture upload and cropping (TODO)</p>
-    <p>Showing a Bootstrap modal (TODO)</p>
-    <p>Using a date picker (TODO)</p>
-    <p>Navbar (TODO)</p>
+    <div class="row pt-3">
+      <div class="col">Dynamic nested form</div>
+      <div class="col">TODO</div>
+    </div>
 
-    <p>Feel free to add a PR if you think that an example is missing.</p>
+    <div class="row pt-3">
+      <div class="col">Multistep wizard form</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-3">
+      <div class="col">File upload</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-3">
+      <div class="col">File upload to S3</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-3">
+      <div class="col">Picture upload and cropping</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-3">
+      <div class="col">Showing a Bootstrap modal</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-3">
+      <div class="col">Using a date picker</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-3">
+      <div class="col">Navbar</div>
+      <div class="col">TODO</div>
+    </div>
+
+    <div class="row pt-5">
+      <div class="col">Feel free to add a PR if you think that an example is missing.</div>
+    </div>
     """
   end
 
