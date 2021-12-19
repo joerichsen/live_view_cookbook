743629833dfc63a27c8afa995ed80911233225fa
Jørgen Orehøj Erichsen <joe@erichsen.net>
2021-12-19 17:39:26 +0100

    Create a separate page for the front page

--- lib/cookbook_web/live/front_page_live.ex
+++ lib/cookbook_web/live/front_page_live.ex
@@ -3,13 +3,31 @@ defmodule CookbookWeb.FrontPageLive do
 
   def render(assigns) do
     ~H"""
-    <%= raw @readme %>
+    <h1>Welcome to the Phoenix LiveView Cookbook</h1>
+    <h3>This repo contains examples of using Phoenix LiveView</h3>
+
+    <p>Check out the live demo at <a href="https://liveviewcookbook.dev/">https://liveviewcookbook.dev/</a></p>
+
+    <p>The plan is to have the following examples</p>
+
+    <ul>
+      <li><a href="https://liveviewcookbook.dev/hello_world">Hello World</a></li>
+      <li>Form with validation</li>
+      <li>Dynamic nested form</li>
+      <li>Multistep wizard form</li>
+      <li>File upload</li>
+      <li>File upload to S3</li>
+      <li>Picture upload and cropping</li>
+      <li>Showing a Bootstrap modal</li>
+      <li>Using a date picker</li>
+      <li>Navbar</li>
+    </ul>
+
+    <p>Feel free to add a PR if you think that an example is missing.</p>
     """
   end
 
   def mount(_params, _session, socket) do
-    readme = File.read!(Path.join(:code.priv_dir(:cookbook), "README.md"))
-    html = Earmark.as_html!(readme)
-    {:ok, assign(socket, :readme, html)}
+    {:ok, socket}
   end
 end
