948bf5eb558e2144f5d507ee1b7cf00a9e968295
Jørgen Orehøj Erichsen <joe@erichsen.net>
2021-12-19 17:50:15 +0100

    Prettier front page

--- lib/cookbook_web/live/front_page_live.ex
+++ lib/cookbook_web/live/front_page_live.ex
@@ -4,24 +4,25 @@ defmodule CookbookWeb.FrontPageLive do
   def render(assigns) do
     ~H"""
     <h1>Welcome to the Phoenix LiveView Cookbook</h1>
-    <h3>This repo contains examples of using Phoenix LiveView</h3>
+    <h3>This contains examples of using Phoenix LiveView</h3>
 
-    <p>Check out the live demo at <a href="https://liveviewcookbook.dev/">https://liveviewcookbook.dev/</a></p>
+    <p>The plan is to have the following examples:</p>
 
-    <p>The plan is to have the following examples</p>
+    <p>
+      Hello World
+      <a href="https://liveviewcookbook.dev/hello_world" class="btn btn-outline-success ml-5">Demo</a>
+      <a href="https://github.com/joerichsen/live_view_cookbook/blob/main/lib/cookbook_web/live/hello_world_live.ex" class="btn btn-outline-primary">Code</a>
+    </p>
 
-    <ul>
-      <li><a href="https://liveviewcookbook.dev/hello_world">Hello World</a></li>
-      <li>Form with validation</li>
-      <li>Dynamic nested form</li>
-      <li>Multistep wizard form</li>
-      <li>File upload</li>
-      <li>File upload to S3</li>
-      <li>Picture upload and cropping</li>
-      <li>Showing a Bootstrap modal</li>
-      <li>Using a date picker</li>
-      <li>Navbar</li>
-    </ul>
+    <p>Form with validation (TODO)</p>
+    <p>Dynamic nested form (TODO)</p>
+    <p>Multistep wizard form (TODO)</p>
+    <p>File upload (TODO)</p>
+    <p>File upload to S3 (TODO)</p>
+    <p>Picture upload and cropping (TODO)</p>
+    <p>Showing a Bootstrap modal (TODO)</p>
+    <p>Using a date picker (TODO)</p>
+    <p>Navbar (TODO)</p>
 
     <p>Feel free to add a PR if you think that an example is missing.</p>
     """
