6f241f3f2021904e303993659fc86693895a67df
Jørgen Orehøj Erichsen <joe@erichsen.net>
2022-01-23 11:49:40 +0100

    Working pagination

--- lib/cookbook_web/live/table_live.ex
+++ lib/cookbook_web/live/table_live.ex
@@ -12,7 +12,7 @@ defmodule CookbookWeb.TableLive do
         </tr>
       </thead>
       <tbody>
-        <%= for row <- @rows do %>
+        <%= for row <- @rows_on_this_page do %>
           <tr>
             <td><%= row.name %></td>
             <td><%= row.birthday %></td>
@@ -21,12 +21,35 @@ defmodule CookbookWeb.TableLive do
         <% end %>
       </tbody>
     </table>
+
+    <nav>
+      <ul class="pagination">
+        <li class={"page-item #{@page == 1 && "disabled" || ""}"}><a class="page-link" href="#" phx-click="goto_page" phx-value-page={@page - 1}>Previous</a></li>
+        <%= for page <- (1..@total_pages) do %>
+          <li class={"page-item #{page == @page && "active" || ""}"}><a class="page-link" href="#" phx-click="goto_page" phx-value-page={page}><%= page %></a></li>
+        <% end %>
+        <li class={"page-item #{@page == @total_pages && "disabled" || ""}"}><a class="page-link" href="#" phx-click="goto_page" phx-value-page={@page + 1}>Next</a></li>
+      </ul>
+    </nav>
     """
   end
 
   def mount(_params, _session, socket) do
-    socket = socket |> assign_rows() |> assign_sort_variables() |> sort()
+    socket =
+      socket
+      |> assign_rows()
+      |> reset_sort_variables()
+      |> reset_pagination_variables()
+      |> sort()
+      |> paginate()
+
     {:ok, socket}
+  end
+
+  def handle_event("goto_page", %{"page" => page}, socket) do
+    page = String.to_integer(page)
+    socket = assign(socket, page: page) |> paginate()
+    {:noreply, socket}
   end
 
   def handle_event("sort", %{"sort_by" => sort_by, "sort_direction" => sort_direction}, socket) do
@@ -60,13 +83,23 @@ defmodule CookbookWeb.TableLive do
     assign(socket, rows: rows)
   end
 
-  defp assign_sort_variables(socket) do
+  defp paginate(%{assigns: %{page: page, page_size: page_size}} = socket) do
+    total_pages = ceil(length(socket.assigns.rows) / page_size)
+    rows_on_this_page = socket.assigns.rows |> Enum.slice((page - 1) * page_size, page_size)
+    assign(socket, total_pages: total_pages, rows_on_this_page: rows_on_this_page)
+  end
+
+  defp reset_sort_variables(socket) do
     assign(socket, sort_by: :name, sort_direction: :asc)
+  end
+
+  defp reset_pagination_variables(socket) do
+    assign(socket, page: 1, page_size: 10)
   end
 
   defp assign_rows(socket) do
     rows =
-      Enum.map(1..20, fn _ ->
+      Enum.map(1..50, fn _ ->
         %{
           name: Faker.Person.first_name() <> " " <> Faker.Person.last_name(),
           birthday: Faker.Date.date_of_birth(),
